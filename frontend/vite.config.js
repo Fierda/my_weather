14fdd7284d48de8e8487b5a91a8a346742cb2b61
Fierda K. Ariefputra <fierdakurniacap21@gmail.com>
2025-07-07 14:15:18 +0700

    Fix Error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,10 @@
-// frontend/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true,       // ✅ Enables `test`, `expect`, etc. without import
-    environment: 'jsdom' // ✅ Needed for testing React components
+    globals: true,
+    environment: 'jsdom',
   },
 });
