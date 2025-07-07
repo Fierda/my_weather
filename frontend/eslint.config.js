99042a82d89f666f95fb593f91f03abe5ba4d556
Fierda K. Ariefputra <fierdakurniacap21@gmail.com>
2025-07-07 13:50:06 +0700

    Fix Error

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,6 +1,6 @@
-const reactPlugin = require('eslint-plugin-react');
 const tsParser = require('@typescript-eslint/parser');
 const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const reactPlugin = require('eslint-plugin-react');
 
 /** @type {import("eslint").Linter.FlatConfig[]} */
 module.exports = [
@@ -11,14 +11,23 @@ module.exports = [
       parserOptions: {
         ecmaVersion: 2020,
         sourceType: 'module',
-        ecmaFeatures: { jsx: true },
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
     },
     plugins: {
-      '@typescript-eslint': tsPlugin,
-      react: reactPlugin,
+      '@typescript-eslint': tsPlugin.rules,
+      react: reactPlugin.rules,
     },
     rules: {
+      // TypeScript ESLint rules
+      ...tsPlugin.configs.recommended.rules,
+
+      // React rules
+      ...reactPlugin.configs.recommended.rules,
+
+      // Overrides
       'react/react-in-jsx-scope': 'off',
       'react/jsx-uses-react': 'off',
     },
