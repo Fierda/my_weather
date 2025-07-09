fbe705ba46c187825bd113e5ac9cc184b52d5d0f
Fierda K. Ariefputra <fierdakurniacap21@gmail.com>
2025-07-09 14:26:26 +0700

    Fix Error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,5 +16,9 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    coverage: {
+      reporter: ['cobertura', 'text'], 
+      reportsDirectory: 'coverage',   
+    },
   },
 });
